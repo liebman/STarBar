a01f52c0774a338a68dc0be0d416958a9b1dc4dc
Carl Poole <mail@carlpoole.com>
2016-04-11 04:22:53 -0400

    Initial Commit

---
+++ STarBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  STarBar
+//
+//  Created by CARL POOLE on 4/11/16.
+//  Copyright © 2016 Carl Poole. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
