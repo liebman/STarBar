f558046e91673bc6fe64114256a129dfd2fb7a3e
Carl Poole <mail@carlpoole.com>
2016-04-17 02:06:27 -0400

    Initial Menu App Testing

---
+++ STarBar/STarBar/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  STarBar
+//
+//  Created by CARL POOLE on 4/17/16.
+//  Copyright © 2016 Carl Poole. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        let icon = NSImage(named: "statusIcon")
+        icon?.template = true
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+    }
+    
+    @IBAction func menuClicked(sender: NSMenuItem) {
+        
+        let task = NSTask()
+        task.launchPath = "/usr/bin/defaults"
+        
+        if(sender.state == NSOnState){
+            sender.state = NSOffState
+            task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "NO"]
+        }else{
+            sender.state = NSOnState
+            task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "YES"]
+        }
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        let killTask = NSTask()
+        killTask.launchPath = "/usr/bin/killall"
+        killTask.arguments = ["Finder"]
+        killTask.launch()
+        
+    }
+
+
+}
+
